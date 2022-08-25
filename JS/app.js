d921a4480d7a0ff1818dd5c6b7b6e1cb306e156d
josecou88 <josemcourel@gmail.com>
2022-08-24 21:08:36 -0300

    agregando toastify

--- JS/app.js
+++ JS/app.js
@@ -131,3 +131,19 @@ const Toast = Swal.mixin({
 
  //Botón Tostify
 
+ document.querySelector('.butonCTA').addEventListener ('click', ()=> {
+    Toastify({
+       text: "Agregaste tu producto exitosamente ",
+       duration: 4000,
+    //    destination: "https://www.ole.com.ar/messi/",
+       newWindow: true,
+       close: true,
+       gravity: "top", // `top` or `bottom`
+       position: "right", // `left`, `center` or `right`
+       stopOnFocus: true, // Prevents dismissing of toast on hover
+       style: {
+         background: "linear-gradient(to right, #00b09b, #96c93d)",
+       },
+       onClick: function(){} // Callback after click
+     }).showToast();
+   })
